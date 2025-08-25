90ef1fbd1c5ad43343ad68d535b2af93c47a2a12
dpmallia624 <dpmallia@gmail.com>
2025-08-25 18:29:35 +0530

    committed postgres configuration for bucket4j

--- postgres-bucket4j/src/main/java/com/springframework/service/RateLimitBucketService.java
+++ postgres-bucket4j/src/main/java/com/springframework/service/RateLimitBucketService.java
@@ -6,8 +6,10 @@ import io.github.bucket4j.postgresql.PostgreSQLSelectForUpdateBasedProxyManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import static java.time.Duration.ofMinutes;
 import static java.time.Duration.ofSeconds;
 
 @Component
@@ -20,24 +22,24 @@ public class RateLimitBucketService {
     public Bucket bucket(){
         Long key = 1L;
         BucketConfiguration bucketConfiguration = BucketConfiguration.builder()
-                .addLimit(limit -> limit.capacity(10).refillGreedy(10, ofSeconds(1)))
+                .addLimit(limit -> limit.capacity(2).refillGreedy(10, ofMinutes(1)))
                 .build();
         return proxyManager.getProxy(key, () -> bucketConfiguration);
     }
 
-//    private static final int MAX_TO_REMOVE_IN_ONE_TRANSACTION = 1_000;
-//    private static final int THRESHOLD_TO_CONTINUE_REMOVING = 50;
-//
-//    @Scheduled(cron = "0 30 4 * * *")
-//    public void scheduleFixedDelayTask() {
-//        int removedKeysCount = 0, removedCount;
-//        do {
-//            removedCount = proxyManager.removeExpired(MAX_TO_REMOVE_IN_ONE_TRANSACTION);
-//            if (removedKeysCount > 0) {
-//                logger.info("Removed {} expired buckets", removedCount);
-//            } else {
-//                logger.info("There are no expired buckets to remove");
-//            }
-//        } while (removedCount > THRESHOLD_TO_CONTINUE_REMOVING);
-//    }
+    private static final int MAX_TO_REMOVE_IN_ONE_TRANSACTION = 1_000;
+    private static final int THRESHOLD_TO_CONTINUE_REMOVING = 50;
+
+    @Scheduled(cron = "0 30 4 * * *")
+    public void scheduleFixedDelayTask() {
+        int removedKeysCount = 0, removedCount;
+        do {
+            removedCount = proxyManager.removeExpired(MAX_TO_REMOVE_IN_ONE_TRANSACTION);
+            if (removedKeysCount > 0) {
+                logger.info("Removed {} expired buckets", removedCount);
+            } else {
+                logger.info("There are no expired buckets to remove");
+            }
+        } while (removedCount > THRESHOLD_TO_CONTINUE_REMOVING);
+    }
 }
